5c35a0c5514cd3ea55c93fb1bc3dfc5dd77d436a
Cilinski <ceciliawikberg@hotmail.com>
2017-04-19 12:03:57 +0200

    Jobbar med att få highscores version 4

--- extension/popup/highscore.js
+++ extension/popup/highscore.js
@@ -1,24 +1,39 @@
 
-/*document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var elem = document.getElementById("knapp2");
     if (elem) {
-        elem.addEventListener("click", getHighScores());
+        elem.addEventListener("click", getHighscores());
     }
-});*/
+});
 
 /*console.log(response);*/
 
-var data = { startTime: 1490208166633 };
-var userInfomation;
+var data = { "startTime": 1490208166633 };
+var userInformation;
 
-$("#knapp2").click(function() {
+/*$("#knapp2").click(function() {*/
+
+function getHighscores(){
+
     $.ajax({
+        type: "GET",
+        url: "/api/getHighScores",
+        data: "startTime=1490208166633",
+        success: function (data) { console.log(data); },
+        error: function (msg) { console.log(msg); }
+     });
+
+    /*$.ajax({
         type:"GET",
         url: "/api/getHighScores",
-        data: JSON.stringify(data),
+        data: JSON.stringify(data);
         success: function (userInfo)
         {
             userInfomation = userInfo;
+            console.log(userInformation);
         }
-    });
-});
+         error: function (response) {
+
+          }
+});*/
+};
