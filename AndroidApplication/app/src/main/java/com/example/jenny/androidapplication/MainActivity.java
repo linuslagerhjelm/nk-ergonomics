42546747d73386a333c11e4928fbf22e097f7081
Jenny Kinert <id14jkt@cs.umu.se>
2017-04-19 11:49:19 +0200

    Added a function to main activity

--- AndroidApplication/app/src/main/java/com/example/jenny/androidapplication/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/jenny/androidapplication/MainActivity.java
@@ -2,11 +2,40 @@ package com.example.jenny.androidapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
+    private Button okButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addKeyListener();
+    }
+
+    public void addKeyListener() {
+        okButton = (Button) findViewById(R.id.button3);
+        okButton.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                    Toast.makeText(MainActivity.this, okButton.getText(), Toast.LENGTH_LONG).show();
+                    return true;
+
+                } else if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_9)) {
+
+                    Toast.makeText(MainActivity.this, "Number nine is pressed!", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
