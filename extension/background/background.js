a27d78918c5f03fa350f8ec9b6614467698bdd9b
Viktor Ahlund <id14vad@cs.umu.se>
2017-05-04 12:36:28 +0200

    Pushar för att Cias skit är borta

--- extension/background/background.js
+++ extension/background/background.js
@@ -2,3 +2,30 @@
  * Created by linuslagerhjelm on 2017-02-28.
  * Recreated by Cilinksi
  */
+
+ console.log("hej2");
+
+data = {
+                   "firstName": "Linus",
+                   "lastName": "Lagerhjelm",
+                   "office": "LA"
+               }
+
+/*$.ajax({
+    type: "POST",
+    url: "http://localhost/api/createUser",
+    data: JSON.stringify(data),
+    success: function (data) { console.log(data) },
+});*/
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Action to be performed when the document is read;
+       console.log(this.responseText)
+    }
+};
+xhttp.open("GET", "/api/getUsers?office=STOCKHOLM", true);
+xhttp.send();
+
+
